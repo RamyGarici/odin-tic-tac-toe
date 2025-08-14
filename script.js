81da3fd831b44a1d33993e9b3559379dac916d8f
RamyGarici <ramymanilgarici@gmail.com>
2025-08-14 11:15:16 +0100

    player and WIN_LINES

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = (()=>{
+const gameboard = (()=>{
 const _board = new Array(9).fill(null);
 
 const getBoard = ()=> _board.slice();
@@ -12,7 +12,15 @@ const placeMark = (index, mark)=>{
     _board[index] = mark;
     return{ok:true};
 
-}
+};
+ return { getBoard, reset, isFull, placeMark };
 
 
-})
+})();
+
+const player = (name,mark) =>{return(name,mark)};
+const WIN_LINES = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6]
+];
